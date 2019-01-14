16b76adac5b5596fbd0513cc831a6291fec1aa0f
Ben Surgison <ben@surgison.net>
2019-01-14 19:01:27 +0000

    Improve errors

--- classes/Game.js
+++ classes/Game.js
@@ -10,10 +10,26 @@ module.exports = (function () {
     }
 
     addPlayer (player) {
-      const game = privateProps.get(this)
-      if (game.players.includes(player)) {
+      const { players } = privateProps.get(this)
+      if (players.includes(player)) {
         throw new GameError('Player already in the game')
       }
+      players.push(player)
+    }
+
+    removePlayer (player) {
+      const game = privateProps.get(this)
+      const { players } = game
+      if (!players.includes(player)) {
+        throw new GameError('Player has already left the game')
+      }
+      players.splice(players.indexOf(player), 1)
+    }
+
+    withPlayers (fn) {
+      const game = privateProps.get(this)
+      const { players } = game
+      players.forEach((player) => fn(player))
     }
   }
 
